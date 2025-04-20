3c01e552b9ae62cd6ec60a8c8581d9ae4f2c7591
jjeris <johans.justs.eris@gmail.com>
2025-04-20 18:59:15 +0300

    Add FE code

---
+++ src/components/TitleBar/TitleBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './TitleBar.css';
+
+const TitleBar = () => {
+  return (
+    <div className="titlebar">
+      <span className="title">Blendio-Tauri</span>
+    </div>
+  );
+};
+
+export default TitleBar;
